20277c0f9ee14844e0cbae22cb9cd47f697e7fff
kz <zaramaluna1999@hotmail.com>
2020-06-18 14:17:25 -0500

    button to change the color of the li

--- example/src/components/User.js
+++ example/src/components/User.js
@@ -1,20 +1,41 @@
 import React from "react";
 
-// class User extends React.Component {
-//   render() {
-//     const name = this.props.name;
-//     return <li>{name}</li>;
-//   }
-// }
+// Class component
+class User extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "#cccccc",
+    };
+  }
 
+  onPressButton() {
+    this.setState({
+      color: "#abcdef",
+    });
+  }
+
+  render() {
+    const name = this.props.name;
+    return (
+      <li style={{ backgroundColor: this.state.color }}>
+        {name}
+        <button onClick={this.onPressButton.bind(this)}>Change color</button>
+      </li>
+    );
+  }
+}
+
+// // Function component
 // function User(props) {
 //   return <li>{props.name}</li>;
 // }
 
-const User = (props) => (
-  <div>
-    <li>{props.name}</li>
-  </div>
-);
+// // Arrow function component
+// const User = (props) => (
+//   <div>
+//     <li>{props.name}</li>
+//   </div>
+// );
 
 export default User;
