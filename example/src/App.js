6aca416370341bc9fc01751957975f569725505f
kz <zaramaluna1999@hotmail.com>
2020-06-18 12:41:56 -0500

    show a list of animals

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      name: "Dog",
+    },
+    {
+      id: 2,
+      name: "Cat",
+    },
+    {
+      id: 3,
+      name: "Fish",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {users.map((user, index) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
