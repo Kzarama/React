a08a4178eca55545cb742553438574b3387f3911
kz <zaramaluna1999@hotmail.com>
2020-06-25 10:46:59 -0500

    separation of view and logic detail

---
+++ attendance/src/pages/BadgeDetailsContainer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import api from "../api";
+import Loading from "../components/Loading";
+import PageError from "../components/PageError";
+
+import BadgeDetails from "./BadgeDetails";
+
+class BadgeDetailsContainer extends React.Component {
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId);
+
+      this.setState({ loading: false, data: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    if (this.state.error) {
+      return <PageError />;
+    }
+
+    return <BadgeDetails badge={this.state.data} />;
+  }
+}
+
+export default BadgeDetailsContainer;
