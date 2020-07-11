339e2482ee5dd542a8ed79af0761926fad7a13e2
kz <zaramaluna1999@hotmail.com>
2020-07-11 12:49:37 -0500

    status added

--- rick_and_morthy/src/components/CharacterDetails.js
+++ rick_and_morthy/src/components/CharacterDetails.js
@@ -9,10 +9,24 @@ function CharacterDetails(props) {
   }
 
   return ReactDOM.createPortal(
-    <div className="Modal">
-      <div className="Modal__container">
+    <div className="Detail">
+      <div className="Detail_container">
         <h1>{props.character.name}</h1>
-        <button onClick={props.onClose} className="Modal__close-button">
+        <img src={props.character.image} alt={props.character.name} />
+        {props.character.status === "Alive" ? (
+          <h2>
+            Status = <span class="dot-alive"></span>
+          </h2>
+        ) : props.character.status === "Dead" ? (
+          <h2>
+            Status = <span class="dot-dead"></span>
+          </h2>
+        ) : (
+          <h2>
+            Status = <span class="dot-unknown"></span>
+          </h2>
+        )}
+        <button onClick={props.onClose} className="Detail_close-button">
           Close
         </button>
       </div>
