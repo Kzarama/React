b44613f345aadc9e69f6f4c6fc485cdf54ef3cb2
kz <zaramaluna1999@hotmail.com>
2020-07-10 19:58:58 -0500

    component renamed

--- rick_and_morthy/src/components/Characters.js
+++ rick_and_morthy/src/components/Characters.js
@@ -2,16 +2,11 @@ import React from "react";
 import "./styles/characters.css";
 
 class Characters extends React.Component {
-  info = () => {
-    console.log(this.props.character.name);
-  };
-
   render() {
     return (
       <div
         className="character"
         style={{ backgroundImage: `url(${this.props.character.image})` }}
-        onClick={() => this.info()}
       >
         <div className="name_container">
           <span>{this.props.character.name}</span>
