14c8f023913802ba84faf5bd7b75cff0144ce38c
kz <zaramaluna1999@hotmail.com>
2020-07-18 12:03:21 -0500

    publicaciones section added

---
+++ redux_p/src/actions/publicacionesActions.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { TRAER_POR_USUARIO } from "../types/publicacionesTypes";
+
+export const traerPorUsuario = (key) => async (dispatch, getState) => {
+  const { usuarios } = getState().usuariosReducer;
+  const { publicaciones } = getState().publicacionesReducer;
+  const usuario_id = usuarios[key].id;
+
+  const respuesta = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
+  );
+
+  const publicaciones_actualizadas = [...publicaciones, respuesta.dad];
+
+  dispatch({
+    type: TRAER_POR_USUARIO,
+    payload: publicaciones_actualizadas,
+  });
+};
