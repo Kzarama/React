d4620e81e827f00c9916754dbe06c1644b7c17c0
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-07-20 15:52:40 -0500

    tareas types added

--- redux_p/src/types/tareasTypes.js
+++ redux_p/src/types/tareasTypes.js
@@ -1,3 +1,6 @@
 export const TRAER_TODAS = "tareas_traer_todos";
 export const CARGANDO = "tareas_cargando";
 export const ERROR = "tareas_ERROR";
+export const CAMBIO_USUARIO_ID = "tareas_cambio_usuario_id";
+export const CAMBIO_TITULO = "tarea_cambio_titulo";
+export const AGREGADA = "tarea_agregada";
