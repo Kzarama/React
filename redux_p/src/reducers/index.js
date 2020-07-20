d8ce8ad80c22ddc27a0f156fe2f20b84df96be08
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-07-19 22:37:05 -0500

    tareas reducer added

--- redux_p/src/reducers/index.js
+++ redux_p/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import usuariosReducer from "./usuariosReducer";
 import publicacionesReducer from "./publicacionesReducer";
+import tareasReducer from "./TareasReducer";
 
 export default combineReducers({
   usuariosReducer,
   publicacionesReducer,
+  tareasReducer,
 });
